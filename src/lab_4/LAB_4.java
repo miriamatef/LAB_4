0de78eb5a5a7cb4870324939db62847794ce9f46
miriamatef <miriamatef25@gmail.com>
2025-10-15 18:38:25 +0300

    start project

---
+++ src/lab_4/LAB_4.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_4;
+import java.time.LocalDate;
+//MY FILE
+
+/**
+ *
+ * @author DELL
+ */
+public class LAB_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
